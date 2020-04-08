4dbf6d6f6d93849fd3cee851c7d599f649da9725
ayushgupta6387 <ayush.gupta1_cs18@gla.ac.in>
2020-04-08 20:05:22 +0530

    fix the amount for making one cup coffee

--- src/stage3/CoffeeMachine.java
+++ src/stage3/CoffeeMachine.java
@@ -5,5 +5,8 @@ import java.util.Scanner;
 public class CoffeeMachine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int water = 200;
+        int milk = 50;
+        int beans = 15;
     }
 }
