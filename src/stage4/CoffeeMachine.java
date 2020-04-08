c4a1e4cfb9edc00c017aef80e1ac38ffa9ad0d9f
ayushgupta6387 <ayush.gupta1_cs18@gla.ac.in>
2020-04-08 22:24:40 +0530

    create a method display

--- src/stage4/CoffeeMachine.java
+++ src/stage4/CoffeeMachine.java
@@ -11,6 +11,11 @@ public class CoffeeMachine {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+    }
+
+    static public void summary() {
 
     }
+
 }
+
