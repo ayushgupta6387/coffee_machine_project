0f717ee088fef9f6ba1e186edb42fb8ab74580af
ayushgupta6387 <ayush.gupta1_cs18@gla.ac.in>
2020-04-08 23:46:32 +0530

    give choices to user to select any one

--- src/stage5/Main.java
+++ src/stage5/Main.java
@@ -1,8 +1,15 @@
 package stage5;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Boolean exit = false;
         CoffeeMachine obj = new CoffeeMachine();
-
+        do {
+            System.out.print("Write action (buy, fill, take, remaining, exit): ");
+            String action = scanner.next();
+        } while (exit == false);
     }
 }
