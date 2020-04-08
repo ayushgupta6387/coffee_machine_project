6c0fba4584ce20ded2f6cf30f76f9b871ce540d1
ayushgupta6387 <ayush.gupta1_cs18@gla.ac.in>
2020-04-08 23:21:09 +0530

    set all the values of a required fields

--- src/stage5/CoffeeMachine.java
+++ src/stage5/CoffeeMachine.java
@@ -4,11 +4,15 @@ import java.util.Scanner;
 
 public class CoffeeMachine {
     Scanner scanner = new Scanner(System.in);
+    int water = 400;
+    int milk = 540;
+    int beans = 120;
+    int cups = 9;
+    int money = 550;
 
     public void buy() {
         System.out.println();
         System.out.print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ");
         String choice = scanner.next();
-
     }
 }
