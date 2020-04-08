1cdcbfa401ad02257af9d4d56b8baddec506336e
ayushgupta6387 <ayush.gupta1_cs18@gla.ac.in>
2020-04-08 20:04:29 +0530

    import scanner class for taking input

---
+++ src/stage3/CoffeeMachine.java
@@ -0,0 +1,9 @@
+package stage3;
+
+import java.util.Scanner;
+
+public class CoffeeMachine {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
