a9fe148d13f41f7e0db25c6466be7460566e5a1f
ayushgupta6387 <ayush.gupta1_cs18@gla.ac.in>
2020-04-08 20:10:56 +0530

    take input of how many grams of beans machine has

--- src/stage3/CoffeeMachine.java
+++ src/stage3/CoffeeMachine.java
@@ -12,5 +12,7 @@ public class CoffeeMachine {
         int waterLevel = scanner.nextInt();
         System.out.print("Write how many ml of milk the coffee machine has: ");
         int milkLevel = scanner.nextInt();
+        System.out.print("Write how many grams of coffee beans the coffee machine has: ");
+        int beanLevel = scanner.nextInt();
     }
 }
