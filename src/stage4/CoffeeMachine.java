0a1ec8addc00eadb18b404309c7f1a5ddc16b055
ayushgupta6387 <ayush.gupta1_cs18@gla.ac.in>
2020-04-08 22:22:32 +0530

    import the scanner class to take the input

--- src/stage4/CoffeeMachine.java
+++ src/stage4/CoffeeMachine.java
@@ -1,9 +1,16 @@
 package stage4;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
     static int water = 1200;
     static int milk = 540;
     static int beans = 120;
     static int cups = 9;
     static int money = 550;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
 }
