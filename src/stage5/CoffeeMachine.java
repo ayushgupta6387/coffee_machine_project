df112a56d3c4994182e73c0df6782385ed5a28b7
ayushgupta6387 <ayush.gupta1_cs18@gla.ac.in>
2020-04-09 14:56:20 +0530

    define the required fields

--- src/stage5/CoffeeMachine.java
+++ src/stage5/CoffeeMachine.java
@@ -2,4 +2,11 @@ package stage5;
 
 public class CoffeeMachine {
 
+    static int water = 400;
+    static int milk = 540;
+    static int beans = 120;
+    static int cups = 9;
+    static int money = 550;
+
+    static Boolean exit = false;
 }
