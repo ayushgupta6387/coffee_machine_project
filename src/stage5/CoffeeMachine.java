2e7e035b2f31192e4a6f0299c998570c447a974e
ayushgupta6387 <ayush.gupta1_cs18@gla.ac.in>
2020-04-09 14:59:49 +0530

    create method buy and take for which variety

--- src/stage5/CoffeeMachine.java
+++ src/stage5/CoffeeMachine.java
@@ -1,5 +1,7 @@
 package stage5;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
 
     static int water = 400;
@@ -9,4 +11,15 @@ public class CoffeeMachine {
     static int money = 550;
 
     static Boolean exit = false;
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
+
+    static void buy() {
+        System.out.println();
+        System.out.print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ");
+        String choice = scanner.next();
+    }
 }
